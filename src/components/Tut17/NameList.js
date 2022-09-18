d509be43d9eb0726c962f2bdb687ad5cac8884e7
Vuthik <kolvongvuthik@gmail.com>
2022-09-18 11:39:04 +1000

    added comments

--- src/components/Tut17/NameList.js
+++ src/components/Tut17/NameList.js
@@ -25,6 +25,13 @@ function NameList() {
   const personList = persons.map((person) => (
     <Person key={person.id} person={person} />
   ));
+  //index as key may cause problems if we do sorting with this list
+  //adding an item at the start of list or sorting may cause values to be 
+  //overwritten 
+  //can use index as key if all below 3 conditions are met:
+  //1: The items in your list does not have a unique id
+  //2: The list is a static list and will not change. No adding or removing items
+  //3: The list will never be reordered or filtered
   const nameList = names.map((name, index) => (
     <h2 key={index}>
       {index}: {name}
